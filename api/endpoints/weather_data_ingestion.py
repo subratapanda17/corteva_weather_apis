8b8022b8544c68cd24ea38c6bc33df7807caebb6
Subrata Panda <subratapanda56@gmail.com>
2024-09-18 21:05:18 +0530

    app blueprints

--- api/endpoints/weather_data_ingestion.py
+++ api/endpoints/weather_data_ingestion.py
@@ -1,17 +1,17 @@
 from flask import Blueprint, jsonify
-from flask_restx import Resource, Api, fields
+from flask_restx import Resource, Api, fields, Namespace
 from api.modules.functions.weather_data_ingestion import InsertWeatherData
 from api.modules.schemas.weather_data_ingestion import weather_data_ingestion_model
 
-# Create a blueprint for the weather data ingestion endpoint
-weather_bp = Blueprint('weather_data_ingestion', __name__)
-api = Api(weather_bp)
 
-@api.route('/weather/ingest')
+weather_ingestion_ns = Namespace('weather_data_ingestion', description='Weather data insert')
+
+
+@weather_ingestion_ns.route('/weather_ingestion')
 class WeatherDataIngest(Resource):
-    @api.expect(weather_data_ingestion_model)
+    @weather_ingestion_ns.expect(weather_data_ingestion_model)
     def get(self):
-        
+        return jsonify("into weather ingestion")
         result = InsertWeatherData().insert_weather_data()
         
         return jsonify(result)
--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 from flask import Flask
-from flask_restx import Api
-from api.endpoints.weather_data_ingestion import weather_bp
+from flask_restx import Api, Namespace, Resource
+# from api.endpoints.get_weather_data import weather_data_ns
+# from api.endpoints.get_weather_stats import weather_stats_ns
+from api.endpoints.weather_data_ingestion import weather_ingestion_ns
 
-# Create the Flask app
 app = Flask(__name__)
 
-# Initialize Flask-RESTX API
-api = Api(app)
+# Initialize Flask-RESTX Api
+api = Api(
+    app,
+    version="1.0",
+    title="Weather API",
+    description="An API for retrieving and processing weather data.",
+    # doc="/docs"  # Swagger UI available at /docs
+)
 
-# Register the blueprint
-app.register_blueprint(weather_bp)
 
-@app.route('/')
-def home():
-    return "API is running!"
+test_ns = Namespace('test', description="Test Namespace")
 
-# Run the app
+@test_ns.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {"message": "Hello, World!"}
+
+api.add_namespace(test_ns, path='/api')
+
+# Register namespaces   
+# api.add_namespace(weather_data_ns, path='/weather')
+# api.add_namespace(weather_stats_ns, path='/weather/stats')
+api.add_namespace(weather_ingestion_ns, path='/api/')
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
